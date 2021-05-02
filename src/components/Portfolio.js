3e8a3195eb97d15015a37eb42d54d0accf9f25a8
Mr. Spencer <codekingkyle@gmail.com>
2021-05-02 19:18:18 -0400

    af

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -13,7 +13,7 @@ export default class Porfolio extends Component {
               return(
                 <div className="columns portfolio-item">
                   <div className="item-wrap">
-                    <a href={item.url}>
+                    <a href={item.url} >
                       <img src={`${item.imgurl}`} className="item-img"/>
                       <div className="overlay">
                         <div className="portfolio-item-meta">
@@ -22,6 +22,7 @@ export default class Porfolio extends Component {
                         </div>
                       </div>
                     </a>
+                    <a href={item.url} ></a>
                   </div>
                 </div>
               )
